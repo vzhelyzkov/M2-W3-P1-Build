e903421fa5d737ade702569bdeb88bd8abc1a7a8
vzhelyzkov <92399454+vzhelyzkov@users.noreply.github.com>
2022-01-27 19:57:31 +0200

    Add files via upload

---
+++ buildTime.js
@@ -0,0 +1,41 @@
+//Shopping cart and ambassadors - BuildTime #2
+
+/*
+Details:    Today you are in charge of building the shopping cart logic for a website that supports ambassadors. 
+            Your job is to build a program flow that will elaborate a list of prices, a user and a shipping cost to calculate the correct cart total. 
+
+            If the user is an ambassador, the cart should be discounted of 30% BEFORE shipping cost (ambassadors still pay shipping)
+            If the user is NOT an ambassador, the cart should NOT be discounted
+            Either way, if the cart goes over 100, shipping cost should be 0.
+
+            Some users as examples are already provided.
+            
+           
+*/
+
+const marco = {
+    name: "Marco",
+    lastName: "Rossi",
+    isAmbassador: true,
+  };
+  
+  const paul = {
+    name: "Paul",
+    lastName: "Flynn",
+    isAmbassador: false,
+  };
+  
+  const amy = {
+    name: "Amy",
+    lastName: "Reed",
+    isAmbassador: false,
+  };
+  
+  const prices = [34, 5, 2];
+  const shippingCost = 50;
+  const total = 0;
+
+if (isAmbassador = true) {
+
+}
+???????????????????????
